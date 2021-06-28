3fd7416ca618ea5f36598f8ff6fce7c2c22153bd
Zachary Goodman <znote89@gmail.com>
2021-06-28 13:23:17 -0400

    commit tree 0e551f2b33548b80edf9b12ee2b9d439f67eca87

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let result;
+  if (feet <= 400) {
+    result = 'This one is on me!'
+  } else if (feet > 2000 && feet < 2499) {
+    result = 'I will gladly take your thirty bucks.'
+  } else if (feet > 2500) {
+    result = 'No can do.'
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let result;
+  result = city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  return result;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let result;
+  switch (tip) {
+    case tip = 'generous': result = 'Thank you so much.';
+    break;
+    case tip = 'not as generous': result = 'Thank you.';
+    break;
+    default: result = 'Bye.';
+  }
+  return result;
 }
